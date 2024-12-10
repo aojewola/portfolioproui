045c26b07572ecc5d1d4c67e99b3346ec83b748c
Asimi Ojewola <asimi.ojewola@gmail.com>
2024-12-10 22:57:06 +0000

    Added project setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'portfolioproui' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolioproui');
+    expect(app.title).toEqual('PortfolioPro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolioproui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PortfolioPro');
   });
 });
